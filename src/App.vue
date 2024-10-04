29e27f93bbe1a804c718de305856cc8ca82ce090
Du Cheng <easyhoo2426@163.com>
2024-10-04 13:10:46 +0800

    feat: add tailwind css

--- src/App.vue
+++ src/App.vue
@@ -5,25 +5,8 @@ const value = ref('')
 </script>
 
 <template>
-  <div data-tauri-drag-region class="titlebar" />
-  <div class="container">
-    <input v-model="value" placeholder="输入代码片段标题">
+  <div data-tauri-drag-region />
+  <div class="size-full">
+    <input v-model="value" placeholder="输入代码片段标题" class="h-10 w-full font-bold text-xl border-red-300 border-2 rounded-lg p-2">
   </div>
 </template>
-
-<style lang="scss" scoped>
-.container {
-  width: 100%;
-  height: 100%;
-
-  input {
-    height: 40px;
-    width: 98%;
-    margin: 0;
-    padding: 5px;
-    border-radius: 5px;
-    border: 1px solid skyblue;
-    font-size: large;
-  }
-}
-</style>
