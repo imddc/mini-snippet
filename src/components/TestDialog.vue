3f87f1e4f6a27153867f6720ae330013b5a5061b
Du Cheng <easyhoo2426@163.com>
2024-10-04 19:57:17 +0800

    feat: dislog test

--- src/components/TestDialog.vue
+++ src/components/TestDialog.vue
@@ -1,9 +1,45 @@
 <script setup lang="ts">
-console.log(1234)
+import { ask, confirm, message } from '@tauri-apps/plugin-dialog'
+
+async function handleAsk() {
+  const result = await ask('title', {
+    title: 'this is a ask',
+    kind: 'info',
+  })
+
+  console.log(result)
+}
+
+async function handleConfirm() {
+  const result = await confirm('title', {
+    title: 'do you want this?',
+    kind: 'error',
+    okLabel: 'yes',
+    cancelLabel: 'cancel',
+  })
+  console.log(result)
+}
+
+async function handleMessage() {
+  await message('title', {
+    title: 'this is a message',
+    kind: 'warning',
+  })
+}
 </script>
 
 <template>
-  <div />
+  <div class="flex gap-2">
+    <button type="button" @click="handleAsk">
+      ask
+    </button>
+    <button type="button" @click="handleConfirm">
+      confirm
+    </button>
+    <button type="button" @click="handleMessage">
+      message
+    </button>
+  </div>
 </template>
 
 <style scoped></style>
