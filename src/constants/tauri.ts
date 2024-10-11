5327b61947ef2d33819f18f6fef66fc41e257ffa
Du Cheng <easyhoo2426@163.com>
2024-10-12 01:01:20 +0800

    feat: persist code theme

--- src/constants/tauri.ts
+++ src/constants/tauri.ts
@@ -3,4 +3,5 @@ export const TAURI_STORE_NAME = '__app_store.bin'
 export const TAURI_STORE_KEYS = {
   CATEGORIES: 'categories',
   SNIPPETS: 'snippets',
+  CODE_THEME: 'code_theme',
 }
--- src/store/settingStore.ts
+++ src/store/settingStore.ts
@@ -1,10 +1,14 @@
 import type { BundledTheme } from 'shiki'
+import { TAURI_STORE_KEYS } from '@/constants/tauri'
 import { store } from '@/plugins/pinia'
+import { useTauriStore } from '@/utils/tauriStore'
 import { defineStore } from 'pinia'
 
 interface State {
   codeTheme: BundledTheme
 }
+
+const tauriStore = useTauriStore()
 
 export const useSettingStore = defineStore('setting', {
   state: (): State => ({
@@ -13,6 +17,7 @@ export const useSettingStore = defineStore('setting', {
   actions: {
     setCodeTheme(theme: BundledTheme) {
       this.codeTheme = theme
+      tauriStore.set(TAURI_STORE_KEYS.CODE_THEME, theme)
     },
     getCodeTheme() {
       return this.codeTheme
