03f48b247a8526824a8121bc0e4c6d351397c3a0
Du Cheng <easyhoo2426@163.com>
2024-10-11 22:13:43 +0800

    fix: dialog warn

--- src/components/common/CategoryEditDialog.vue
+++ src/components/common/CategoryEditDialog.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import type { CategoryV2 } from '@/types/snippet'
 import { Button } from '@/components/ui/button'
-import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog'
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from '@/components/ui/dialog'
 import { Input } from '@/components/ui/input'
 import { ref } from 'vue'
 import { toast } from 'vue-sonner'
@@ -29,19 +29,20 @@ function change() {
 
 <template>
   <Dialog open>
-    <DialogContent aria-describedby="category-edit-dialog-description">
+    <DialogContent>
       <DialogHeader>
         <DialogTitle>
           {{ category ? 'update category' : 'add category' }}
         </DialogTitle>
+        <DialogDescription>
+          {{ category ? 'update category' : 'add category' }}
+        </DialogDescription>
       </DialogHeader>
-
       <Input
         v-model.trim="value.name"
         placeholder="category name"
         class="mt-4 ring-1 ring-gray-500"
       />
-
       <DialogFooter>
         <Button size="sm" variant="outline" @click="$emit('close')">
           cancel
