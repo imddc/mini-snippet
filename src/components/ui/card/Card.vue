1bb191ac35910ca58df41fcd1ebced35fcc367c0
Du Cheng <easyhoo2426@163.com>
2024-10-14 18:22:12 +0800

    WIP: load local

--- src/components/ui/card/Card.vue
+++ src/components/ui/card/Card.vue
@@ -11,7 +11,7 @@ const props = defineProps<{
   <div
     :class="
       cn(
-        'rounded-lg border bg-card text-card-foreground shadow-sm',
+        'rounded-lg border bg-card/50 text-card-foreground shadow-sm',
         props.class,
       )
     "
--- src/pages/setting.vue
+++ src/pages/setting.vue
@@ -1,28 +1,57 @@
 <script setup lang="ts">
 import type { BundledTheme } from 'shiki'
 import Header from '@/components/content/Header.vue'
+import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Select, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
 import SelectContent from '@/components/ui/select/SelectContent.vue'
 import { Switch } from '@/components/ui/switch'
 import { useSettingStore } from '@/store/settingStore'
 import { disable, enable } from '@tauri-apps/plugin-autostart'
+import { BaseDirectory, readFile } from '@tauri-apps/plugin-fs'
 import { bundledThemesInfo } from 'shiki'
-import { watchEffect } from 'vue'
+import { ref, watch } from 'vue'
+import { toast } from 'vue-sonner'
 
 const settingStore = useSettingStore()
+
+const test = ref<Record<string, string>>({})
 
 function updateCodeTheme(theme: string) {
   settingStore.setCodeTheme(theme as BundledTheme)
 }
 
-watchEffect(async () => {
-  if (settingStore.autoStart) {
+async function loadLocal() {
+  const input = document.createElement('input')
+  input.type = 'file'
+  input.click()
+  input.addEventListener('change', async () => {
+    if (input.files) {
+      const file = input.files[0]
+      if (!file.name.endsWith('.json')) {
+        toast.error('only support json file')
+        return
+      }
+      const text = await file.text()
+      test.value = JSON.parse(text)
+
+      console.log(test.value)
+    }
+  })
+  input.remove()
+}
+
+function saveLocal() {
+}
+
+watch(() => settingStore.autoStart, async (val) => {
+  if (val) {
     await enable()
   }
   else {
-    disable()
+    await disable()
   }
 })
 </script>
@@ -77,6 +106,37 @@ watchEffect(async () => {
               </SelectItem>
             </SelectContent>
           </Select>
+        </CardContent>
+      </Card>
+
+      <!-- 快捷键 -->
+      <Card>
+        <CardHeader>
+          <CardTitle>
+            Shortcut
+          </CardTitle>
+          <CardDescription>input a shortcut</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Input placeholder="input a shortcut" />
+        </CardContent>
+      </Card>
+
+      <!-- 本地化 -->
+      <Card>
+        <CardHeader>
+          <CardTitle>
+            Local
+          </CardTitle>
+          <CardDescription>config local</CardDescription>
+        </CardHeader>
+        <CardContent class="flex items-center gap-2">
+          <Button size="sm" variant="outline" @click="loadLocal">
+            load
+          </Button>
+          <Button size="sm" variant="outline" @click="saveLocal">
+            save
+          </Button>
         </CardContent>
       </Card>
     </div>
